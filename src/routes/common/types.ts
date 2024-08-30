b4ae1260ba3e93ad5cb6e9e44bb7f00e912bde65
koushal <95288059+Koushlendra-Kumar@users.noreply.github.com>
2024-08-30 19:27:12 +0530

    🎉 Initial project setup

---
+++ src/routes/common/types.ts
@@ -0,0 +1,14 @@
+/* eslint-disable max-len */
+/**
+ * Shared types for routes.
+ */
+
+import { Response, Request } from "express";
+
+// **** Express **** //
+
+type TObj = Record<string, unknown>;
+
+export type IReq = Request<TObj, void, TObj, TObj>;
+
+export type IRes = Response<unknown, TObj>;
---
+++ src/server.ts
@@ -0,0 +1,73 @@
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import helmet from "helmet";
+import express, { Request, Response, NextFunction } from "express";
+import logger from "jet-logger";
+
+import "express-async-errors";
+
+import BaseRouter from "@src/routes/PostRoutes";
+
+import Paths from "@src/common/Paths";
+import EnvVars from "@src/common/EnvVars";
+import HttpStatusCodes from "@src/common/HttpStatusCodes";
+import { RouteError } from "@src/common/classes";
+import { NodeEnvs } from "@src/common/misc";
+
+// **** Variables **** //
+
+const app = express();
+
+// **** Setup **** //
+
+// Basic middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(EnvVars.CookieProps.Secret));
+
+// Show routes called in console during development
+if (EnvVars.NodeEnv === NodeEnvs.Dev.valueOf()) {
+  app.use(morgan("dev"));
+}
+
+// Security
+if (EnvVars.NodeEnv === NodeEnvs.Production.valueOf()) {
+  app.use(helmet());
+}
+
+// Add APIs, must be after middleware
+app.use(Paths.Base, BaseRouter);
+
+// Add error handler
+app.use(
+  (
+    err: Error,
+    _: Request,
+    res: Response,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    next: NextFunction,
+  ) => {
+    if (EnvVars.NodeEnv !== NodeEnvs.Test.valueOf()) {
+      logger.err(err, true);
+    }
+    let status = HttpStatusCodes.BAD_REQUEST;
+    if (err instanceof RouteError) {
+      status = err.status;
+    }
+    return res.status(status).json({ error: err.message });
+  },
+);
+
+// Nav to users pg by default
+app.get("/", (_: Request, res: Response) => {
+  return res.redirect("/users");
+});
+
+// Redirect to login if not logged in.
+app.get("/users", (_: Request, res: Response) => {
+  return res.json({ msg: "hello" });
+});
+
+// **** Export default **** //
+
+export default app;
